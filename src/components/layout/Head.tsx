4d15f505a4809ae68508cfbadfc96c3e542d59e1
claykaufmann <clay.kaufmann@gmail.com>
2021-02-15 15:47:14 -0500

    add Head.tsx

---
+++ src/components/layout/Head.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import Helmet from 'next/head'
+
+const Head: React.FC = () => (
+    <Helmet>
+        <title key='title'>Clay Kaufmann</title>
+    </Helmet>
+)
+
+export default Head
