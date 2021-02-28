1ccea34617b750299905a715dd63a742709d5749
claykaufmann <clay.kaufmann@gmail.com>
2021-02-28 15:59:58 -0500

    add some styling to the burger menu, add more text to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react'
 import Base from '@components/layout/Base'
 import { NextPage } from 'next'
-import { Heading, Text } from '@chakra-ui/react'
+import { Center, Heading, Text, VStack } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
     <Base>
-      <Heading size='lg'></Heading>
-      <Text>
-        Hello! My name is Clay Kaufmann. I am studying computer science at the University of
-        Vermont.
-      </Text>
+      <Center h='40vh'>
+        <VStack>
+          <Heading size='lg'>Hey there!</Heading>
+          <Text>
+            My name is Clay Kaufmann, and I an endeavoring software engineer, studying computer
+            science at the University of Vermont.
+          </Text>
+        </VStack>
+      </Center>
     </Base>
   )
 }
