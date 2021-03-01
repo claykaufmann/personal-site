9aef1f92809ac28beac9294812ef3fa3e0ca5ce5
claykaufmann <clay.kaufmann@gmail.com>
2021-03-01 10:34:23 -0500

    move landing page into its own component

--- components/layout/Base.tsx
+++ components/layout/Base.tsx
@@ -3,11 +3,11 @@ import Footer from './Footer'
 import Header from './Header'
 import styles from './Base.module.scss'
 
-const Base: React.FC = props => {
+const Base: React.FC = Props => {
   return (
     <React.Fragment>
       <Header />
-      <div className={styles.content}>{props.children}</div>
+      <div className={styles.content}>{Props.children}</div>
       <Footer />
     </React.Fragment>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,13 @@
 import * as React from 'react'
-import Base from '@components/layout/Base'
 import Footer from '@components/layout/Footer'
 import { NextPage } from 'next'
-import { Button, Center, Heading, Text, VStack } from '@chakra-ui/react'
-import styles from './index.module.scss'
-import Header from '@components/layout/Header'
+import LandingPage from '@components/layout/LandingPage'
 
 const Home: NextPage = () => {
   return (
     <React.Fragment>
-      <div className={styles.landingPage}>
-        <div className={styles.header}>
-          <Header />
-        </div>
-        <div className={styles.mainContent}>
-          <Center h='40vh' className={styles.centerText}>
-            <VStack>
-              <Heading size='lg'>Hey there!</Heading>
-              <Text>
-                My name is Clay Kaufmann, and I an endeavoring software engineer, studying computer
-                science at the University of Vermont.
-              </Text>
-            </VStack>
-          </Center>
-        </div>
-        <Center className={styles.buttonContainer}>
-          <Button className={styles.aboutButton}>About me.</Button>
-        </Center>
-        <Footer />
-      </div>
-      <div className={styles.aboutSection}>
+      <LandingPage />
+      <div>
         <Footer />
       </div>
     </React.Fragment>
