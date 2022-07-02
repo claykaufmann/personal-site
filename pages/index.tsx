eb2ab5b8fc32d3c399f2099bac0996c03f160eaa
Clay Kaufmann <claykaufmann@gmail.com>
2022-07-01 22:01:13 -0700

    clean up text

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,31 +104,30 @@ const Home: NextPage = () => {
               I was born and raised just north of San Francisco, in Marin
               County. Being the son of a software engineer, I had no option but
               to embrace computers and what they could do. From a young age, I
-              quickly embraced computers, and as a result, when I got to
-              college, I decided to pursue computer science and software
+              quickly took a deep interest in computing, and as a result, when I
+              got to college, I decided to pursue computer science and software
               engineering.
             </Text>
             <br />
             <Text>
-              Fast forward 4 years later, and I am currently a Master&apos;s
+              Fast forward to the present, and I am currently a Master&apos;s
               candidate at the University of Vermont, where I continue to study
               computer science. My main interests within the CS field revolve
               around the intersection of networking, DevOps, and general systems
               programming. My expected graduation is December 2022, after which
-              I plan to enter the software engineering industry. If you are
-              interested, let&apos;s{' '}
+              I plan to enter the software engineering industry. Upon entering
+              the industry, I would like to get into the field of site
+              reliability engineering, where my unique combination of interests
+              within the software engineering realm would apply quite well. If
+              you are interested, let&apos;s{' '}
               <Link href="mailto:claykaufmann@gmail.com" color="blue">
-                get in touch.
-              </Link>{' '}
-              Upon entering the industry, I would like to get into the field of
-              site reliability engineering, where my unique combination of
-              interests within the software engineering realm would apply quite
-              well.
+                get in touch!
+              </Link>
             </Text>
           </Box>
 
           <Box margin="4em 2em">
-            <Heading>Development</Heading>
+            <Heading marginBottom={'0.2em'}>Development</Heading>
             <Link href="/projects" style={{ textDecoration: 'none' }}>
               <GitProjectsHomePage
                 repoUrls={[
