5c92002b07098706d60ec7f0c0d8bd95306449c4
Clay Kaufmann <claykaufmann@gmail.com>
2022-07-25 21:28:23 -0400

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Global } from '@emotion/react'
 import theme from '@styles/theme'
 import fonts from '@styles/font-face'
 import { ChakraProvider } from '@chakra-ui/react'
+import Script from 'next/script'
 
 import 'styles/globals.scss'
 
@@ -14,6 +15,20 @@ const App: React.VFC<AppProps> = (props: AppProps) => {
     <ChakraProvider theme={theme}>
       <Global styles={fonts} />
       <Head />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-5EVMWCSQ4X"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </ChakraProvider>
   )
