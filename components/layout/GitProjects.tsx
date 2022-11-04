2cd1205f0f86c6d6f3ce6faa3b9bc5f6e755ba44
Clay Kaufmann <claykaufmann@gmail.com>
2022-11-03 23:27:34 -0400

    get rid of vfc

--- components/layout/GitProjects.tsx
+++ components/layout/GitProjects.tsx
@@ -7,7 +7,7 @@ interface props {
   repos: gitRepoInfo[]
 }
 
-const GitProjects: React.VFC<props> = ({ repos }) => {
+const GitProjects: React.FC<props> = ({ repos }) => {
   return (
     <SimpleGrid columns={[1, 1, 2, 3]} spacing={'1em 5em'}>
       {repos.map((repo) => (
